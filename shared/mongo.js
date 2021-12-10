a529b4f707d20ca2625aab76025450841c60cde2
ANKE-SaiPrasad <89138270+ankesai8@users.noreply.github.com>
2021-12-10 07:27:32 +0530

    Update mongo.js

--- shared/mongo.js
+++ shared/mongo.js
@@ -29,30 +29,4 @@ let mongo = {
     } 
 }
 
-module.exports = mongo;//const DATA_BASE = "computer";
-const client =  new MongoClient(process.env.URL);
-
-let mongo = {
-
-    db:null,
-    laptops:null,
-    async connect()
-    {
-        try{
-             //connecting mongodb
-        await client.connect();
-        console.log("Mongodb connected");
-
-        this.db = client.db(process.env.DATA_BASE);
-        this.laptops=this.db.collection("laptops");
-        console.log("setting the database and collections");
-        }
-        catch(err)
-        {
-            console.log(err);
-        }
-       
-    } 
-}
-
-module.exports = mongo;
+ module.exports = mongo;
